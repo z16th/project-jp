55314d9f64f400e554ceb7e512315f932230ea04
LoLz XVI <47308963+LoLzWTFomfg@users.noreply.github.com>
2020-07-01 17:21:15 -0500

    Improved JSONs for table data
    Delegated grid style to JSON.
    Reduced number of files to 1 for each character type.

--- app/src/components/Syllabaries.js
+++ app/src/components/Syllabaries.js
@@ -1,68 +1,10 @@
 import React from 'react'
 import Table from './Table'
-import kanaSimple from '../utils/kana-simple.json'
-import headersSimple from '../utils/headers-simple.json'
-import kanaDakuten from '../utils/kana-dakuten.json'
-import headersDakuten from '../utils/headers-dakuten.json'
-import kanaCombinations from '../utils/kana-combinations.json'
-import headersCombinations from '../utils/headers-combinations.json'
+import simple from '../utils/simple.json'
+import dakuten from '../utils/dakuten.json'
+import combinations from '../utils/combinations.json'
+
 import './styles/Syllabaries.css'
-
-const simpleGrid = {
-  gridTemplateColumns: `repeat(6, 100px)`,
-  gridTemplateRows: `repeat(12, 100px)`,
-  gridTemplateAreas:
-  `
-  '. xa xi xu xe xo'
-  'y- a i u e o'
-  'yk ka ki ku ke ko'
-  'ys sa shi su se so'
-  'yt ta chi tsu te to'
-  'yn na ni nu ne no'
-  'yh ha hi fu he ho'
-  'ym ma mi mu me mo'
-  'yy ya . yu . yo'
-  'yr ra ri ru re ro'
-  'yw wa . . . wo'
-  'ynn n . . . .'
-  `
-}
-
-const dakutenGrid = {
-  gridTemplateColumns: `repeat(6, 100px)`,
-  gridTemplateRows: `repeat(6, 100px)`,
-  gridTemplateAreas:
-  `
-  '. xa xi xu xe xo'
-  'yg ga gi gu ge go'
-  'yz za ji zu ze zo'
-  'yd da dzi dzu de do'
-  'yb ba bi bu be bo'
-  'yp pa pi pu pe po'
-  `
-}
-
-const combinationsGrid = {
-  gridTemplateColumns: `repeat(4, 100px)`,
-  gridTemplateRows: `repeat(14, 100px)`,
-  gridTemplateAreas:
-  `
-  '. xや xゆ xよ'
-  'yk kya kyu kyo'
-  'ysh sha shu sho'
-  'ych cha chu cho'
-  'yn nya nyu nyo'
-  'yh hya hyu hyo'
-  'ym mya myu myo'
-  'yr rya ryu ryo'
-  '. . . .'
-  'yg gya gyu gyo'
-  'yj ja ju jo'
-  'yd dya dyu dyo'
-  'yb bya byu byo'
-  'yp pya pyu pyo'
-  `
-}
 
 const fonts = [
   'Kosugi Maru',
@@ -98,21 +40,21 @@ export default function Syllabaries(){
       <div id='tables' style={{fontFamily: currentFont}}>
       <Table 
         label={'Caracteres Simples'}
-        dataJSON={kanaSimple} 
-        headersJSON={headersSimple} 
-        styleObj={simpleGrid}
+        dataJSON={simple.kana} 
+        headersJSON={simple.headers} 
+        styleObj={simple.gridStyle}
          />
       <Table 
         label={'Caracteres con Dakuten'}
-        dataJSON={kanaDakuten}
-        headersJSON={headersDakuten}
-        styleObj={dakutenGrid}
+        dataJSON={dakuten.kana}
+        headersJSON={dakuten.headers}
+        styleObj={dakuten.gridStyle}
       />
       <Table 
         label={'Caracteres Combinados'}
-        dataJSON={kanaCombinations}
-        headersJSON={headersCombinations}
-        styleObj={combinationsGrid}
+        dataJSON={combinations.kana}
+        headersJSON={combinations.headers}
+        styleObj={combinations.gridStyle}
       />
       </div>
     </div>
