e4921094f5e0952b8816d36321205de5f0f83cde
LoLz XVI <47308963+LoLzWTFomfg@users.noreply.github.com>
2020-07-03 20:47:26 -0500

    Implemented Kanji early-state style

--- app/src/components/KanjiCard.js
+++ app/src/components/KanjiCard.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import KanjiAnimation from './KanjiAnimation'
+import './styles/KanjiCard.css'
 
 export default function KanjiCard({data}){
   const { utf16, number, strokes, character, meaning} = {...data}
   const [ showAnimation, setShowAnimation ] = React.useState(false)
-  const [ displayType, setDisplayType ] = React.useState('Romaji')
+  const [ displayType, setDisplayType ] = React.useState('Kana')
 
   const handleAnimationClick = () => {
     setShowAnimation(state => !state)
@@ -16,19 +17,27 @@ export default function KanjiCard({data}){
 
   return(
     <div className='kanji-card'>
-      {showAnimation &&
-        <KanjiAnimation name={utf16} />
-      }
-      <div className='kanji-writing' onClick={handleAnimationClick}>
+
+      <div className='kanji-left'>
+        {!showAnimation &&
+          <div className='kanji-char'>{character}</div>
+        }
+        {showAnimation &&
+          <KanjiAnimation name={utf16} width={120} height={120} />
+        }
+        <div className='kanji-strokes'  onClick={handleAnimationClick}>Trazos: {strokes}</div>
         <div className='kanji-number'>No: {number}</div>
-        <div className='kanji-strokes'>Trazos: {strokes}</div>
-        <div className='kanji-char'>{character}</div>
       </div>
-      <div className='kanji-reading' onClick={handleReadingClick}>
-        <div className='kanji-onyomi'>On'yomi: {data[`onyomi${displayType}`]}</div>
-        <div className='kanji-kunyomi'>Kun'yomi: {data[`kunyomi${displayType}`]}</div>
+
+      <div className='kanji-right'>
+        <div className='kanji-reading' onClick={handleReadingClick}>
+          <div className='kanji-onyomi'>On'yomi: {data[`onyomi${displayType}`]}</div>
+          <div className='kanji-kunyomi'>Kun'yomi: {data[`kunyomi${displayType}`]}</div>
+        </div>
+        <br></br>
         <div className='kanji-meaning'>Significado: {meaning}</div>
-      </div>      
+      </div>
+
     </div>
   )
 }
--- app/src/components/KanjiPage.js
+++ app/src/components/KanjiPage.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import kanjiData from '../utils/kanji-1-80.json'
 import KanjiCard from './KanjiCard'
+import './styles/KanjiPage.css'
 
 export default function KanjiPage(){
   return(
     <div id='kanji-page'>
       <h1 className='header'>Una descripción sobre esta página</h1>
-    <div id='kanji-table'>
-      { 
-        kanjiData.map(e => (
-          <KanjiCard key={e.utf16} data={e} />
-        ))
-      }
-    </div>
+      <div id='kanji-table'>
+        { 
+          kanjiData.map(e => (
+            <KanjiCard key={e.utf16} data={e} />
+          ))
+        }
+      </div>
+      <p>Los diagramas de trazos están basados en datos proporcionados por <a href='http://kanjivg.tagaini.net/'>KanjiVG</a></p>
+      <p>Parte de la información de este sitio está basada en los libros de referencia</p>
+      <p>{`Seely, C., Henshall, K. G., & Fan, J. (2016). The Complete Guide to Japanese Kanji: (JLPT All Levels) Remembering and Understanding the 2,136 Standard Characters (First ed.) [E-book]. Tuttle Publishing. https://www.amazon.com/-/es/Complete-Guide-Japanese-Kanji-Understanding-ebook/dp/B01DIF7RBI/`}</p>
     </div>
   )
 }
