5dd393a740aaa273e01389740ed4fbc906632318
LoLz XVI <47308963+LoLzWTFomfg@users.noreply.github.com>
2020-07-27 22:32:48 -0500

    Pre style.

---
+++ app/src/utils/colors.js
@@ -0,0 +1,18 @@
+export const blue = {
+  regular: "#82b1ff",
+  light: "#b6e3ff",
+  dark: "#4d82cb",
+  background: "#e3f2fd",
+}
+export const yellow = {
+  regular: "#ffcf82",
+  light: " #ffffb3",
+  dark: "#ca9e54",
+  background: "#fff4e1",
+}
+export const pink = {
+  regular: "#ff82b2",
+  light: "#ffb4e4",
+  dark: "c95183",
+  background: "#ffe2ed",
+}
