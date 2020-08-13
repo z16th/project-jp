46b6aea5785c6e1c2290873aafd3092e4354a893
LoLz XVI <47308963+LoLzWTFomfg@users.noreply.github.com>
2020-08-13 02:54:35 -0500

    Fixed lint warning

--- app/src/components/Japanify.js
+++ app/src/components/Japanify.js
@@ -4,7 +4,7 @@ import JapaneseChar from "./JapaneseChar"
 export const Japanify = ({ word, furigana }) => {
   const chars = [...word]
   const element = []
-  chars.map((char, i) => {
+  chars.forEach((char, i) => {
     element.push(<JapaneseChar key={char} char={char} furigana={furigana[i]} />)
   })
   return [...element]
