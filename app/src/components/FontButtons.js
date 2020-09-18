d50a7f4f2f9ea3a2a66fc5ab999673b5db560fc5
LoLz XVI <47308963+LoLzWTFomfg@users.noreply.github.com>
2020-09-18 03:58:30 -0500

    Fix PractiKana font buttons active state bug

--- app/src/components/FontButtons.js
+++ app/src/components/FontButtons.js
@@ -4,29 +4,31 @@ import React from "react"
 import { jsx } from "@emotion/core"
 import { fontButtons } from "../styling"
 
+const fonts = ["Mk POP", "Asobi Memogaki", "Komorebi Gothic", "Umeboshi"]
+
 export default function FontButtons({ currentFonts, setCurrentFonts }) {
-  const fonts = ["Mk POP", "Asobi Memogaki", "Komorebi Gothic", "Umeboshi"]
   const handleClick = (index) => {
-    if (currentFonts.includes(fonts[index])) {
-      const newArr = currentFonts.filter((e) => e !== currentFonts[index])
-      setCurrentFonts(newArr)
-    } else {
+    if (!currentFonts.includes(fonts[index])) {
       let newArr = [...currentFonts]
       newArr.push(fonts[index])
       setCurrentFonts(newArr)
+    } else {
+      const newArr = currentFonts.filter((e) => e !== fonts[index])
+      setCurrentFonts(newArr)
     }
   }
+
   return (
     <div className="font-buttons" css={fontButtons}>
       {fonts.map((e, i) => (
         <button
-          key={fonts[i]}
+          key={e}
           type="button"
           style={{ fontFamily: fonts[i], fontSize: "3rem" }}
           className={currentFonts.includes(fonts[i]) ? "active" : ""}
           onClick={() => handleClick(i)}
         >
-          お
+          あ
         </button>
       ))}
     </div>
